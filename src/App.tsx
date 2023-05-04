e2658e07df15ec2e5361a3b15fd2c27c5b17cabb
AlysonDEV <devalyson@gmail.com>
2023-05-04 20:15:46 -0300

    Iniciado o projeto

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+
+export function App() {
+
+  return (
+    <main>
+      <section>
+        <header>
+          <h2>Rey Skywalker</h2>
+          <p>Era uma catadora de sucata que descobriu ser sensível à Força durante sua busca ao lendário Mestre Jedi Luke Skywalker.</p>
+        </header>
+        <article>
+          <div>
+            <h3>Filmes</h3>
+            <p>The Force Awakens, The Last Jedi, The Rise of Skywalker & Forces of Destiny</p>
+          </div>
+
+          <div>
+            <h3>Espécie</h3>
+            <p>Humana</p>
+          </div>
+
+          <div>
+            <h3>Altura</h3>
+            <p>1,7m</p>
+          </div>
+
+          <div>
+            <h3>Localidade</h3>
+            <p>Jakku</p>
+          </div>
+
+          <div>
+            <h3>Armas</h3>
+            <p>Sabre de luz, Blaster, Quarterstaff</p>
+          </div>
+        </article>
+      </section>      
+    </main>
+  )
+}
+
