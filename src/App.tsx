8756489958dcf029130e551423ef6912f53880d5
AlysonDEV <devalyson@gmail.com>
2023-05-07 08:52:23 -0300

    Criardo um html padrão de um card

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,48 @@
+import "./app.scss";
+import char from './assets/char_rey.png';
+
 
 export function App() {
 
+
   return (
-    <main>
-      <section>
-        <header>
-          <h2>Rey Skywalker</h2>
-          <p>Era uma catadora de sucata que descobriu ser sensível à Força durante sua busca ao lendário Mestre Jedi Luke Skywalker.</p>
-        </header>
-        <article>
-          <div>
-            <h3>Filmes</h3>
-            <p>The Force Awakens, The Last Jedi, The Rise of Skywalker & Forces of Destiny</p>
-          </div>
+    <>
+      <main>
+        <section>
+          <header>
+            <h2>Rey Skywalker</h2>
+            <p>Era uma catadora de sucata que descobriu ser sensível à Força durante sua busca ao lendário Mestre Jedi Luke Skywalker.</p>
+          </header>
+          <article>
+            <div>
+              <h3>Filmes</h3>
+              <p>The Force Awakens, The Last Jedi, The Rise of Skywalker & Forces of Destiny</p>
+            </div>
 
-          <div>
-            <h3>Espécie</h3>
-            <p>Humana</p>
-          </div>
+            <div>
+              <h3>Espécie</h3>
+              <p>Humana</p>
+            </div>
 
-          <div>
-            <h3>Altura</h3>
-            <p>1,7m</p>
-          </div>
+            <div>
+              <h3>Altura</h3>
+              <p>1,7m</p>
+            </div>
 
-          <div>
-            <h3>Localidade</h3>
-            <p>Jakku</p>
-          </div>
+            <div>
+              <h3>Localidade</h3>
+              <p>Jakku</p>
+            </div>
 
-          <div>
-            <h3>Armas</h3>
-            <p>Sabre de luz, Blaster, Quarterstaff</p>
-          </div>
-        </article>
-      </section>      
-    </main>
+            <div>
+              <h3>Armas</h3>
+              <p>Sabre de luz, Blaster, Quarterstaff</p>
+            </div>
+          </article>
+          <img src={char} alt="char"/>
+        </section>      
+      </main>
+    </>
   )
 }
 
